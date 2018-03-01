0916b11ce2a6f5339cbd4de369ff3cffebf6fd4b
Mo Firouz <mo@heroiclabs.com>
2018-03-01 16:02:54 +0000

    Add tests for link-unlink and notifications

---
+++ test/client-link.test.js
@@ -0,0 +1,83 @@
+/**
+ * Copyright 2018 The Nakama Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const fs = require("fs");
+const TIMEOUT = 5000;
+
+// util to generate a random id.
+const generateid = () => {
+  return [...Array(30)].map(() => Math.random().toString(36)[3]).join('');
+};
+
+describe('Link / Unlink Tests', () => {
+  let page;
+
+  beforeAll(async () => {
+    page = await global.__BROWSER__.newPage();
+
+    page.on("console", msg => console.log("PAGE LOG:", msg.text()));
+    page.on("error", err => console.log("PAGE LOG ERROR:", err));
+    page.on("pageerror", err => console.log("PAGE LOG ERROR:", err));
+
+    const nakamaJsLib = fs.readFileSync(__dirname + "/../dist/nakama-js.umd.js", "utf8");
+    await page.evaluateOnNewDocument(nakamaJsLib);
+    await page.goto("about:blank");
+  }, TIMEOUT);
+
+  it('should link device ID', async () => {
+    const customid = generateid();
+    const deviceid = generateid();
+
+    const account = await page.evaluate((customid, deviceid) => {
+      const client = new nakamajs.Client();
+      return client.authenticateCustom({ id: customid })
+        .then(session => {
+          return client.linkDevice({ id: deviceid }, session)
+            .then(bool => {
+              return client.getAccount(session);
+            });
+        });
+    }, customid, deviceid);
+
+    expect(account).not.toBeNull();
+    expect(account.custom_id).not.toBeNull();
+    expect(account.devices[0]).not.toBeNull();
+  });
+
+  it('should unlink device ID', async () => {
+    const customid = generateid();
+    const deviceid = generateid();
+
+    const account = await page.evaluate((customid, deviceid) => {
+      const client = new nakamajs.Client();
+      return client.authenticateCustom({ id: customid })
+        .then(session => {
+          return client.linkDevice({ id: deviceid }, session)
+            .then(bool => {
+              return client.unlinkDevice( {id: deviceid }, session)
+                .then(bool => {
+                  return client.getAccount(session);
+                })
+            });
+        });
+    }, customid, deviceid);
+
+    expect(account).not.toBeNull();
+    expect(account.custom_id).not.toBeNull();
+    expect(account.hasOwnProperty("devices")).toBe(false);
+  });
+
+}, TIMEOUT);
