04d22be4c9a66e5fd629dcd387eddce4f51884d4
Chris Molozian <chris@heroiclabs.com>
2018-03-01 02:23:54 +0000

    Add TypeScript client and generated code.

---
+++ src/client.ts
@@ -0,0 +1,90 @@
+/**
+ * Copyright 2018 The Nakama Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  ApiAccount,
+  ApiAccountCustom,
+  ApiSession,
+  ApiRpc,
+  ConfigurationParameters,
+  NakamaApi,
+} from "./api.gen";
+
+import { Session } from "./session";
+import { Socket } from "./socket";
+
+const DEFAULT_HOST = "127.0.0.1";
+const DEFAULT_PORT = "7349";
+const DEFAULT_SERVER_KEY = "defaultkey";
+
+/** A client for Nakama server. */
+export class Client {
+  // The low level API client for Nakama server.
+  private apiClient: any;
+  // The server configuration.
+  private configuration: ConfigurationParameters;
+  // The client session.
+  private session?: Session;
+
+  constructor(
+      readonly serverkey = DEFAULT_SERVER_KEY,
+      readonly host = DEFAULT_HOST,
+      readonly port = DEFAULT_PORT,
+      public useSSL = false,
+      public verbose = false) {
+    const scheme = (useSSL) ? "https://" : "http://";
+    const basePath = `${scheme}${host}:${port}`;
+    this.configuration = {
+      username: serverkey,
+      basePath: basePath
+    };
+    this.apiClient = NakamaApi(this.configuration);
+  }
+
+  /** Authenticate a user with a custom id against the server. */
+  authenticateCustom(request: ApiAccountCustom): Promise<Session> {
+    return this.apiClient.authenticateCustom(request).then((apiSession: ApiSession): Session => {
+      const sessionObj = Session.restore(apiSession.token || "");
+      this.session = sessionObj;
+      return sessionObj;
+    });
+  }
+
+  /** A socket created with the client's configuration. */
+  createSocket(session?: Session): Socket {
+    return {
+      session: session
+    };
+  }
+
+  /** Fetch the current user's account. */
+  getAccount(session?: Session): Promise<ApiAccount> {
+    this.configuration.bearerToken = (session && session.token) || (this.session && this.session.token);
+    return this.apiClient.getAccount();
+  }
+
+  /** Execute a Lua function on the server. */
+  rpcFunc(id: string, input: object, session?: Session): Promise<ApiRpc> {
+    this.configuration.bearerToken = (session && session.token) || (this.session && this.session.token);
+    return this.apiClient.rpcFunc(id, JSON.stringify(input));
+  }
+
+  /** Execute a Lua function on the server. */
+  rpcFunc2(id: string, session?: Session): Promise<ApiRpc> {
+    this.configuration.bearerToken = (session && session.token) || (this.session && this.session.token);
+    return this.apiClient.rpcFunc2(id);
+  }
+}
---
+++ src/session.ts
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2017 The Nakama Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export class Session {
+  private constructor(
+    readonly token: string,
+    readonly createdAt: number,
+    readonly expiresAt: number,
+    readonly username: string,
+    readonly userId: string) {
+  }
+
+  isexpired(currenttime: number): boolean {
+    return (this.expiresAt - currenttime) < 0;
+  }
+
+  static restore(jwt: string): Session {
+    const createdAt = Math.floor(new Date().getTime() / 1000);
+    const parts = jwt.split('.');
+    if (parts.length != 3) {
+      throw 'jwt is not valid.';
+    }
+    const decoded = JSON.parse(atob(parts[1])); // FIXME: use base64 polyfill for React Native.
+    const expiresAt = Math.floor(parseInt(decoded['exp']));
+
+    return new Session(jwt, createdAt, expiresAt, decoded['usn'], decoded['uid']);
+  }
+}
