22248fdf412c240e610d9cb67b7405bbf911c769
Blaise <blaiseroth@gmail.com>
2019-07-08 14:03:47 +0930

    Fix typescript bindings for matchmaking messages.
    Map<K, V> is actually an ES6 type and doesn't correspond to { K: V } at all. This was corrected so that this API can be used easily from within a TypeScript environment.
    string_properties and numeric_properties were also made optional in matchmaker_add. I think this is correct as they are left off in an example in the documentation.

--- dist/socket.d.ts
+++ dist/socket.d.ts
@@ -95,8 +95,8 @@ export interface MatchmakerAdd {
         min_count: number;
         max_count: number;
         query: string;
-        string_properties: Map<string, string>;
-        numeric_properties: Map<string, number>;
+        string_properties?: { [key: string]: string };
+        numeric_properties?: { [key: string]: number };
     };
 }
 export interface MatchmakerRemove {
@@ -106,8 +106,8 @@ export interface MatchmakerRemove {
 }
 export interface MatchmakerUser {
     presence: Presence;
-    string_properties: Map<string, string>;
-    numeric_properties: Map<string, number>;
+    string_properties: { [key: string]: string };
+    numeric_properties: { [key: string]: number };
 }
 export interface MatchmakerMatched {
     ticket: string;
