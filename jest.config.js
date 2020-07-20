ddbd1f4707ce414543009606af23e1a2adefbb8f
Luke Gehorsam <lugehorsam@gmail.com>
2020-07-20 13:08:35 -0400

    update dependencies required to use typescript in test suite

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
-module.exports = {
-  "preset": "jest-puppeteer"
-};
+const merge = require('merge')
+const ts_preset = require('ts-jest/jest-preset')
+const puppeteer_preset = require('jest-puppeteer/jest-preset')
+
+//use multiple jest presets by merging and exporting them as a single object
+module.exports = merge.recursive(ts_preset, puppeteer_preset, {
+    globals: {
+      'ts-jest': {
+        tsConfig: 'tsconfig.test.json'
+      }
+    }
+  }
+)
