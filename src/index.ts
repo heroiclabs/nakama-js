b137f166226d16b572d623a43250f01513fbbdf6
Luke Gehorsam <lugehorsam@gmail.com>
2020-07-26 13:38:04 -0400

    export web socket adapters from index

--- src/index.ts
+++ src/index.ts
@@ -19,3 +19,6 @@ import "whatwg-fetch";
 
 export * from "./client";
 export * from "./session";
+export * from "./web_socket_adapter";
+export * from "./web_socket_adapter_pb";
+export * from "./web_socket_adapter_text";
