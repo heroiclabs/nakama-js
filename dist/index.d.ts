aad8e8afd662cac76a6c3f5794a1c11e341f5adf
Luke Gehorsam <lugehorsam@gmail.com>
2020-07-27 12:33:01 -0400

    remove a couple more .d.ts fileS

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import "Base64";
-import "whatwg-fetch";
-export * from "./client";
-export * from "./session";
-export * from "./web_socket_adapter";
-export * from "./web_socket_adapter_pb";
-export * from "./web_socket_adapter_text";
