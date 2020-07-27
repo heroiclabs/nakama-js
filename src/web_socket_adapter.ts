178351b36f1b4d9a02094da6f454f77707b7a824
Luke Gehorsam <lugehorsam@gmail.com>
2020-07-27 10:46:38 -0400

    Additiona licenses and comments

--- src/web_socket_adapter.ts
+++ src/web_socket_adapter.ts
@@ -1,8 +1,42 @@
+/**
+ * Copyright 2020 The Nakama Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * An interface used by Nakama's web socket to determine the payload protocol. 
+ */
 export interface WebSocketAdapter {
     
+    /**
+     * Dispatched when the web socket closes.
+     */
     onClose : SocketCloseHandler | null;
+
+    /**
+     * Dispatched when the web socket receives an error.
+     */
     onError : SocketErrorHandler | null;
+
+    /**
+     * Dispatched when the web socket receives a normal message.
+     */
     onMessage : SocketMessageHandler | null;
+
+    /**
+     * Dispatched when the web socket opens.
+     */
     onOpen : SocketOpenHandler | null;
 
     readonly isConnected: boolean;
@@ -27,6 +61,9 @@ export interface SocketOpenHandler {
     (this : WebSocket, evt : Event) : void
 }
 
+/**
+ * A text-based socket adapter that accepts and transmits payloads over UTF-8.
+ */
 export class WebSocketAdapterText implements WebSocketAdapter {
 
     private _isConnected: boolean = false;
@@ -93,7 +130,7 @@ export class WebSocketAdapterText implements WebSocketAdapter {
     send(msg: any): void {
         if (msg.match_data_send)
         {
-            // TODO document why this is necessary? 
+            // TODO document why this is necessary, if at all.
             msg.match_data_send.op_code = msg.match_data_send.op_code.toString();
         }
         
--- src/web_socket_adapter_pb.ts
+++ src/web_socket_adapter_pb.ts
@@ -1,6 +1,25 @@
+/**
+ * Copyright 2020 The Nakama Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { WebSocketAdapter, SocketCloseHandler, SocketErrorHandler, SocketMessageHandler, SocketOpenHandler } from './web_socket_adapter'
 import * as tsproto from "./github.com/heroiclabs/nakama-common/rtapi/realtime"
 
+/**
+ * A protocol buffer socket adapter that accepts and transmits payloads using the protobuf binary wire format.
+ */
 export class WebSocketAdapterPb implements WebSocketAdapter {
 
     private _isConnected: boolean = false;
