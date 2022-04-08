c7c857c940ca63d74f48b0a662d4f42521530e33
Luke Gehorsam <luke@heroiclabs.com>
2022-04-08 13:37:58 -0400

    update type definitions

--- packages/nakama-js/dist/session.d.ts
+++ packages/nakama-js/dist/session.d.ts
@@ -13,20 +13,31 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/** A nakama session. */
+/** A session authenticated for a user with Nakama server. */
 export interface ISession {
     /** Claims */
+    /** The authorization token used to construct this session. */
     token: string;
+    /** If the user account for this session was just created. */
     created: boolean;
+    /** The UNIX timestamp when this session was created. */
     readonly created_at: number;
+    /** The UNIX timestamp when this session will expire. */
     expires_at?: number;
+    /** The UNIX timestamp when the refresh token will expire. */
     refresh_expires_at?: number;
+    /** Refresh token that can be used for session token renewal. */
     refresh_token: string;
+    /** The username of the user who owns this session. */
     username?: string;
+    /** The ID of the user who owns this session. */
     user_id?: string;
+    /** Any custom properties associated with this session. */
     vars?: object;
     /** Validate token */
+    /** If the session has expired. */
     isexpired(currenttime: number): boolean;
+    /** If the refresh token has expired. */
     isrefreshexpired(currenttime: number): boolean;
 }
 export declare class Session implements ISession {
